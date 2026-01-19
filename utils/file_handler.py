52f0c2f0a082d3833c1d04b3cac345e6885458d1
rakeshreddykundhur01 <rakeshreddykundhur01@gmail.com>
2026-01-19 18:37:45 +0530

    Implemented file handling logic for reading and parsing sales data

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,10 +1,5 @@
 def read_sales_data(filename):
-    """
-    Reads sales data from file handling encoding issues
-
-    Returns:
-        list of raw lines (strings)
-    """
+    
     encodings = ['utf-8', 'latin-1', 'cp1252']
     lines = []
 
